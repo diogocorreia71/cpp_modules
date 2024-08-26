ab9aeb4869475376cae2d3b6589ae6139e874bce
Diogo Correia <diogo_correia7@hotmail.com>
2024-08-26 22:45:35 +0100

    ex01

---
+++ cpp01/ex01/Zombie.cpp
@@ -0,0 +1,23 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+	this->_name = name;
+	std::cout << "Zombie " << this->_name << " created" << std::endl;
+}
+
+Zombie::Zombie(void){}
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie " << this->_name << " is destroyed!" << std::endl;
+}
+
+void Zombie::announce()
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+void Zombie::setName(std::string name)
+{
+	this->_name = name;
+}
---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+Zombie *zombieHorde(int N, std::string name);
+
+int main()
+{
+	int size;
+
+	size = 20;
+	Zombie *horde = zombieHorde(size, "Zom");
+	for (int i = 0; i < size; i++)
+		horde[i].announce();
+	delete[] horde;
+}
