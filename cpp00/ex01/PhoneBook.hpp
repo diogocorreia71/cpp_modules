6e183f4f7da8efa6b44d5966a18f51308fdb4270
Diogo Andre Dos Santos Correia <diodos-s@c2r4s6.42porto.com>
2024-09-04 11:10:23 +0100

    ex01

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 09:40:13 by diodos-s          #+#    #+#             */
+/*   Updated: 2024/09/04 11:00:11 by diodos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
-#include <iostream>
 #include "Contact.hpp"
 
 class PhoneBook
@@ -11,7 +22,6 @@ class PhoneBook
 		Contact contacts[8];
 		void add();
 		void search();
-		void exit();
 
 	private:
 		int id;
