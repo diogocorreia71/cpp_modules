664bf502942b733fdf5667f9c323453761eb310a
Diogo Andre Dos Santos Correia <diodos-s@c2r4s6.42porto.com>
2024-09-06 10:35:56 +0100

    fix ex00 forbidden header

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 09:24:35 by diodos-s          #+#    #+#             */
+/*   Updated: 2024/09/06 09:24:36 by diodos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 
@@ -13,5 +25,9 @@ class Zombie
 	private:
 		std::string _name;
 };
+
+Zombie *newZombie(std::string name);
+
+void randomChump(std::string name);
 
 #endif
