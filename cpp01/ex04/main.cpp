8802541910609db8a618fe730cc1a1410e2e2d55
Diogo Andre Dos Santos Correia <diodos-s@c2r4s6.42porto.com>
2024-09-16 10:03:19 +0100

    fix cpp01_ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 13:48:43 by diodos-s          #+#    #+#             */
-/*   Updated: 2024/09/13 12:03:26 by diodos-s         ###   ########.fr       */
+/*   Updated: 2024/09/16 10:02:59 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char **argv)
 {
@@ -20,32 +21,48 @@ int main(int argc, char **argv)
 		std::cout << "Invalid number of arguments" << std::endl;
 		return 1;
 	}
-	std::ifstream inFile;
-	std::string s1;
-	std::string s2;
-
-	inFile.open(argv[1]);
-	s1 = argv[2];
-	s2 = argv[3];
-	if (inFile.fail())
+	
+	std::ifstream inFile(argv[1]);
+	if (!inFile.is_open())
 	{
-		std::cout << "Error opening file." << std::endl;
+		std::cout << "Error opening file" << std::endl;
 		return 1;
 	}
-	std::string line;
-	getline(inFile, line);
-	size_t pos = 0;
-	while ((pos = line.find(s1, pos)) != std::string::npos)
+
+	inFile.seekg(0, std::ios::end);
+	if (inFile.tellg() == 0)
 	{
-		line.erase(pos, s1.length());
-		line.insert(pos, s2);
-		pos += s2.length();
+		std::cout << "Error: The input file is empty" << std::endl;
+		return 1;
+	}
+	inFile.seekg(0, std::ios::beg);
+	
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	std::string outputFileName = argv[1];
+	outputFileName += ".replace";
+	
+	std::ofstream outFile(outputFileName.c_str());
+	if (!outFile.is_open())
+	{
+		std::cout << "Error opening output file" << std::endl;
+		return 1;
+	}
+	
+	std::string line;
+	while (getline(inFile, line))
+	{
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+		}
+		outFile	<< line << std::endl;
 	}
 	inFile.close();
-
-	std::ofstream outFile;
-	outFile.open("file.replace.txt");
-	outFile << line << std::endl;
 	outFile.close();
 
 	return 0;
