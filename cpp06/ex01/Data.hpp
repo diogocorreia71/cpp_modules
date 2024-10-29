df7818aeb8bd7a5c3cc74ba850eb9ce45365e018
Diogo Andre Dos Santos Correia <diodos-s@c1r3s5.42porto.com>
2024-10-29 09:54:35 +0000

    cpp06 final

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 11:22:22 by diodos-s          #+#    #+#             */
-/*   Updated: 2024/10/29 09:38:06 by diodos-s         ###   ########.fr       */
+/*   Created: 2024/10/29 09:36:16 by diodos-s          #+#    #+#             */
+/*   Updated: 2024/10/29 09:37:37 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERIALIZER_HPP
-#define SERIALIZER_HPP
+#ifndef DATA_HPP
+#define DATA_HPP
 
 #include <iostream>
-#include <stdint.h>
-#include "Data.hpp"
 
-class Serializer
+struct Data
 {
-	private:
-		Serializer();
-		Serializer(const Serializer &other);
-		Serializer &operator=(const Serializer &other);
-		~Serializer();
-	
-	public:
-		static uintptr_t serialize(Data* ptr);
-		static Data* deserialize(uintptr_t raw);
+	int value;
+	std::string name;	
 };
 
 #endif
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,11 +6,12 @@
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 12:36:41 by diodos-s          #+#    #+#             */
-/*   Updated: 2024/10/22 13:44:37 by diodos-s         ###   ########.fr       */
+/*   Updated: 2024/10/29 09:38:17 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+#include "Data.hpp"
 
 int main()
 {
@@ -18,17 +19,19 @@ int main()
 	myData.value = 42;
 	myData.name = "Serialization example";
 
-	std::cout << "Original Data:" << std::endl;
+	std::cout << "<--------Original Data-------->" << std::endl;
 	std::cout << "Value: " << myData.value << std::endl;
 	std::cout << "Name: " << myData.name << std::endl;
 
 	uintptr_t raw = Serializer::serialize(&myData);
-
+	std::cout << std::endl;
+	
 	std::cout << "Serialized Data (raw): " << raw << std::endl;
+	std::cout << std::endl;
 	
 	Data* deserializedData = Serializer::deserialize(raw);
 
-	std::cout << "Deserialized Data:" << std::endl;
+	std::cout << "<--------Deserialized Data-------->" << std::endl;
 	std::cout << "Value: " << deserializedData->value << std::endl;
 	std::cout << "Name: " << deserializedData->name << std::endl;
 
