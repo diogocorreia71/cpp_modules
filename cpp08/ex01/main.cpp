a5df90967904f06aabeef4f49128b5cf8030969d
Diogo Andre Dos Santos Correia <diodos-s@c1r3s5.42porto.com>
2024-11-12 15:28:33 +0000

    cpp08 finished

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 08:57:11 by diodos-s          #+#    #+#             */
-/*   Updated: 2024/11/11 15:23:59 by diodos-s         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:14:57 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,23 @@
 
 int main()
 {
+	std::cout << "Subject main:" << std::endl;
+	
+	Span sp = Span(5);
+	
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	std::cout << std::endl;
+	
 	std::srand(static_cast<unsigned int>(std::time(0)));
 
-	Span sp(10000);
+	Span sp_ind(10000);
 
 	std::vector<int> largeVec;
 	for (int i = 0; i < 10000; i++)
@@ -26,27 +40,16 @@ int main()
 
 	try
 	{
-		sp.addNumbers(largeVec.begin(), largeVec.end());
+		sp_ind.addNumbers(largeVec.begin(), largeVec.end());
 		std::cout << "Added 10,000 numbers to Span successfully" << std::endl;
 
-		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-		std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+		std::cout << "Shortest span: " << sp_ind.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << sp_ind.longestSpan() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << "Exception: " << e.what() << '\n';
 	}
-	
-	Span sp_ind = Span(5);
-	
-	sp_ind.addNumber(6);
-	sp_ind.addNumber(3);
-	sp_ind.addNumber(17);
-	sp_ind.addNumber(9);
-	sp_ind.addNumber(11);
-	
-	std::cout << sp_ind.shortestSpan() << std::endl;
-	std::cout << sp_ind.longestSpan() << std::endl;
 	
 	return 0;
 }
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 09:43:19 by diodos-s          #+#    #+#             */
-/*   Updated: 2024/11/11 12:55:55 by diodos-s         ###   ########.fr       */
+/*   Updated: 2024/11/12 15:27:28 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,18 @@ int main()
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
+	std::cout << "Top element: " << mstack.top() << std::endl;
+	std::cout << "Stack size: " << mstack.size() << std::endl;
 
 	mstack.pop();
+	
 	std::cout << "After popping one element: ";
 	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
-
 	std::cout << "Top element: " << mstack.top() << std::endl;
-	
 	std::cout << "Stack size: " << mstack.size() << std::endl;
 	
 	return 0;
